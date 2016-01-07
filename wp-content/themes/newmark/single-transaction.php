8e9d9dfd2c6b9247cd3845854c700f28eb6a4eaa
Will <whcarter3@gmail.com>
2016-01-06 16:10:50 -0800

    added programmtic property type

--- wp-content/themes/newmark/single-transaction.php
+++ wp-content/themes/newmark/single-transaction.php
@@ -1,4 +1,4 @@
-<h2>Transactions</h2>
+<h2 class="page-title">Transactions</h2>
 <a href="<?php bloginfo('url'); ?>/transactions">< Back to more transactions</a>
 
 <div class="row">
@@ -10,11 +10,35 @@
 			<h3>$<?php the_field('amount'); ?></h3>
 			<h4>For <?php the_title(); ?> <br>
 			in <?php the_field('location'); ?></h4>
-			<p>Financing Type: </p>
-			<p>Property Type: </p>
-			<p>Term: </p>
-			<p>Amortized: </p>
-			<p>Transaction Contacts: </p>
+
+			<?php
+				$field = get_field_object('property_type');
+				$value = get_field('property_type');
+				$label = $field['choices'][$value];
+			?>
+			<table>
+				<tr>
+					<th>Financing Type: </th>
+					<td><?php the_field('financing_type'); ?></td>
+				</tr>
+				<tr>
+					<th>Property Type: </th>
+					<td><?php echo $label; ?></td>
+				</tr>
+				<tr>
+					<th>Term: </th>
+					<td><?php the_field('term'); ?></td>
+				</tr>
+				<tr>
+					<th>Amortized: </th>
+					<td><?php the_field('amortized'); ?></td>
+				</tr>
+			</table>
+			<!-- <p>Financing Type: <?php the_field('financing_type'); ?></p>
+			<p>Property Type:  <?php the_field('property_type'); ?></p>
+			<p>Term: <?php the_field('term'); ?></p>
+			<p>Amortized: <?php the_field('amortized'); ?></p>
+			<p>Transaction Contacts: </p> -->
 
 			<h5>Share this Transaction</h5>
 		</div>
--- wp-content/themes/newmark/templates/transactions-page.php
+++ wp-content/themes/newmark/templates/transactions-page.php
@@ -3,25 +3,61 @@
  * Template Name: Transactions Template
  */
 ?>
+
 <div class="row">
 	<div class="col-md-12">
-		<h1><?php echo get_the_title(); ?></h1>
+		<h2 class="page-title"><?php echo get_the_title(); ?></h2>
 	</div>
 </div>
 <div class="row">
 	<?php $loop = new WP_Query( array( 'post_type' => 'transaction', 'posts_per_page' => -1) ); ?>
 	<?php while ($loop->have_posts()) : $loop->the_post(); ?>
+	<?php
+		// if(get_field('property_type') == 'industrial'){
+		// 	$transaction_class = 'industrial';
+		// 	$transaction_text = 'Indust.';
+		// }
+		$transaction_type = get_field('property_type');
+		switch ($transaction_type) {
+			case 'industrial':
+				$transaction_class = 'industrial';
+				$transaction_text = 'Indust.';
+				break;
+			case 'mixed':
+				$transaction_class = 'mixed';
+				$transaction_text = 'Mixed';
+				break;
+			case 'multi':
+				$transaction_class = 'multi-family';
+				$transaction_text = 'Multi';
+				break;
+			case 'retail':
+				$transaction_class = 'retail';
+				$transaction_text = 'Retail';
+				break;
+			case 'office':
+				$transaction_class = 'office';
+				$transaction_text = 'Indust.';
+				break;
+			default:
+				$transaction_class = 'default';
+				$transaction_text = 'default';
+				break;
+		}
+	 ?>
 		<a href="<?php the_permalink(); ?>">
 			<div class="col-md-3">
 				<figure class="transaction-card">
 					<img class="img-responsive" src="<?php the_post_thumbnail_url('medium'); ?>">
-					<h5><?php the_field('amount'); ?></h5>
-					<p>For <?php the_title(); ?></p>
-					<p>in <?php the_field('location'); ?></p>
+					<figcaption>
+						<h3>$<?php the_field('amount'); ?></h3>
+						<p>For <?php the_title(); ?></p>
+						<p>in <?php the_field('location'); ?></p>
+					</figcaption>
 					<div class="row">
 						<div class="col-md-12">
-							<div class="transaction-type">
-								<p class="transation-type-text">Retail</p>
+							<div class="transaction-type <?php echo $transaction_class; ?>">
+								<p class="transaction-type-text"><?php echo $transaction_text; ?></p>
 							</div>
 						</div>
 					</div>
