5054ac525ab4e6d7f2daea4b6fcf674a6f4c0e37
Dan Hanasono <dan@looking.la>
2016-01-26 16:00:22 -0800

    media query fix to services

--- wp-content/themes/newmark/templates/_about-sections/services.php
+++ wp-content/themes/newmark/templates/_about-sections/services.php
@@ -26,35 +26,45 @@
 			        <div role="tabpanel" class="tab-pane active" id="mortgage-banking">
 			        	<div class="row">
 			        		<div class="col-md-offset-1 col-md-10">
-			        			<div class="mortgage-content"><?php the_field('mortgage_banking'); ?></div>
+			        			<div class="mortgage-content-iphone5-landscape">
+				        			<div class="mortgage-content"><?php the_field('mortgage_banking'); ?></div>
+			        			</div>
 			        		</div>
 			        	</div>
 			        </div>
 			        <div role="tabpanel" class="tab-pane" id="loan-servicing">
 			        	<div class="row">
 			        		<div class="col-md-offset-1 col-md-10">
-			        			<div class="loan-content"><?php the_field('loan_servicing'); ?></div>
+			        			<div class="loan-content-iphone5-landscape">
+				        			<div class="loan-content"><?php the_field('loan_servicing'); ?></div>
+			        			</div>
 			        		</div>
 			        	</div>
 			        </div>
 			        <div role="tabpanel" class="tab-pane" id="debt-advisory">
 						<div class="row">
 			        		<div class="col-md-offset-1 col-md-10">
-			        			<div class="debt-content"><?php the_field('debt_advisory'); ?></div>
+			        			<div class="debt-content-iphone5-landscape">
+				        			<div class="debt-content"><?php the_field('debt_advisory'); ?></div>
+			        			</div>
 			        		</div>
 			        	</div>
 			        </div>
 			        <div role="tabpanel" class="tab-pane" id="acquisition-brokerage">
 			        	<div class="row">
 			        		<div class="col-md-offset-1 col-md-10">
-			        			<div class="acquisition-content"><?php the_field('acquisition_brokerage'); ?></div>
+			        			<div class="acquisition-content-iphone5-landscape">
+				        			<div class="acquisition-content"><?php the_field('acquisition_brokerage'); ?></div>
+			        			</div>
 			        		</div>
 			        	</div>
 			        </div>
 			        <div role="tabpanel" class="tab-pane" id="consulting">
 						<div class="row">
 			        		<div class="col-md-offset-1 col-md-10">
-			        			<div class="consulting-content"><?php the_field('consulting'); ?></div>
+			        			<div class="consulting-content-iphone5-landscape">
+				        			<div class="consulting-content"><?php the_field('consulting'); ?></div>
+			        			</div>
 			        		</div>
 			        	</div>
 			        </div>
