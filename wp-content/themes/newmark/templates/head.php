5ce2fea4f1d8f297218789e88abc19978d7a9bad
dresosh <contact@dresosh.com>
2016-07-06 12:36:14 -0700

    adds newmark favicon to site

--- wp-content/themes/newmark/templates/head.php
+++ wp-content/themes/newmark/templates/head.php
@@ -3,4 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico">
 </head>
